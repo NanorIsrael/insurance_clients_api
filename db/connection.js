085200345bc277057810d540addb26cc90b9de48
nanorissy <israel@alstergh.com>
2022-01-20 23:19:01 +0000

    incidents api to perform post and get requests

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,7 @@ const pgp = require('pg-promise')(initComponents);
 
 const cn = {
         host: 'ec2-3-212-143-188.compute-1.amazonaws.com',
-        port: '5432',
+        port: 5432,
         database: 'dbrcffl9n4mov2',
         user: 'guclxasktufdol',
         password: '4ce687d4322f29e7c7b8fb5e49b8170278226d7b14bfca7a44ebdda3240aea2e',
