e35ac1efc97262d54937f3618507aacf5aeec385
nanorissy <israel@alstergh.com>
2022-01-20 22:42:43 +0000

    incidents api to perform post and get requests

--- db/connection.js
+++ db/connection.js
@@ -9,11 +9,11 @@ const pgp = require('pg-promise')(initComponents);
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const cn = {
-        host: process.env.HOST,
-        port: process.env.PORT,
-        database: process.env.DATABASE,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
+        host: 'ec2-3-212-143-188.compute-1.amazonaws.com',
+        port: 5432,
+        database: 'dbrcffl9n4mov2',
+        user: 'guclxasktufdol',
+        password: '4ce687d4322f29e7c7b8fb5e49b8170278226d7b14bfca7a44ebdda3240aea2e',
         max: 30,
         ssl:true
 }
