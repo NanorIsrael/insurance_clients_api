91e9ae858f101a10903e22e89d0ef10b514ec66c
nanorissy <israel@alstergh.com>
2022-01-20 23:29:59 +0000

    incidents api to perform post and get requests

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,19 @@
 const bbp = require('bluebird');
 
+
 const initComponents = {
     promiseLib: bbp,
 };
-
 const pgp = require('pg-promise')(initComponents);
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const cn = {
-        host: 'ec2-3-212-143-188.compute-1.amazonaws.com',
-        port: 5432,
-        database: 'dbrcffl9n4mov2',
-        user: 'guclxasktufdol',
-        password: '4ce687d4322f29e7c7b8fb5e49b8170278226d7b14bfca7a44ebdda3240aea2e',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         max: 30,
         ssl:true
 }
@@ -20,10 +22,10 @@ const db = pgp(cn);
 
 db.func('version')
     .then(data => {   
-    console.log('Database connection success', typeof process.env.DB_HOST)
+    console.log('Database connection success', data)
     })
     .catch(error => {
-        console.log('Database connection error', typeof process.env.DB_HOST)
+        console.log('Database connection error')
     });
 
 module.exports =  db;
